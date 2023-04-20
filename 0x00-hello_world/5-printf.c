39aa8ec61a2acbd73367bf4dfca92d4c3a9e9a63
banyedeme <banyedeme0@gmail.com>
2023-04-20 08:17:25 -0700

    Printing a function using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+* main - Printing  using the function printf with a new line
+*
+* Return: 0  to indicate (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
