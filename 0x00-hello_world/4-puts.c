2f4ef672dd80df52f3c51b63e3a6f04c863eb590
banyedeme <banyedeme0@gmail.com>
2023-04-20 08:12:07 -0700

    Using puts to print to output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Where the main functions are executed
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
