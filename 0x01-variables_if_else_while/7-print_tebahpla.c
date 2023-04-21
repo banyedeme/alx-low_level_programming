c4deaa0c090648bbd243c316c56c5ae009bb0d25
banyedeme <banyedeme0@gmail.com>
2023-04-21 14:00:42 -0700

    prints the lowercase alphabet in reverse, followed by a new line.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+/**
+ * main - outputs the lowercase alphabet in rev,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
