399be192fe4f943d8fc838496541469e7c26d2f4
banyedeme <banyedeme0@gmail.com>
2023-04-25 10:01:18 -0700

    print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *print_alphabet -  Write a function the print lowercase letter
+  *Folllowed by a newline
+  */
+void print_alphabet(void)
+	{
+		char wyt;
+
+
+		for (wyt = 'a'; wyt <= 'z'; wyt++)
+			_putchar(wyt);
+
+
+		_putchar('\n');
+	}
+
+
