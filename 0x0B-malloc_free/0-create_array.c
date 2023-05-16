45dc90a8421f66bb13de2c692ce470c99eadd7fc
banyedeme <banyedeme0@gmail.com>
2023-05-16 08:24:58 -0700

    creating malloc free memory allocations

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars and init it with a specific char
+ * @size: size of array
+ * @c: char to initialize the array with
+ * Return: On success, returns a pointer to the created array.
+ * Returns NULL if size is 0 or if memory allocation fails.
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	char *array = malloc(size * sizeof(char));
+
+	if (array == (NULL);
+			{
+			return (NULL);
+			}
+			for (unsigned int n = 0; n < size; n++)
+			{
+			array[n] = c;
+			}
+	return (array);
+
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+0x0B. C - malloc, free
+this task is about malloc - dynamic memory allocation.
+
