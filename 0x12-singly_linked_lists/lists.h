f6eda9b5251f1c2f962301103f2f78dff3cb7106
banyedeme <banyedeme0@gmail.com>
2023-06-01 12:53:46 +0000

    singly linked lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,4 @@
 
-lists.h
-
 #ifndef LISTS_H
 #define LISTS_H
 
@@ -8,8 +6,6 @@ lists.h
  * struct list_s - The singly linked list
  * @len: to indicate string length
  * @next: points to next node
- *
-
  * @str: string - the stringc for malloc
  */
 typedef struct list_s
@@ -24,6 +20,7 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 
 #endif
 
