01fe22e7c6328bc1aee87adf147e3a082b321eb6
banyedeme <banyedeme0@gmail.com>
2023-06-01 13:55:58 +0000

    function that prints phrases

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void isprint(void) __attribute__ ((constructor));
+void first(void) __attribute__ ((constructor));
 
 /**
- * isprint - prints a phrases before the main
+ * first - prints a phrases before the main
  * function is executed
  */
-void isprint(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
